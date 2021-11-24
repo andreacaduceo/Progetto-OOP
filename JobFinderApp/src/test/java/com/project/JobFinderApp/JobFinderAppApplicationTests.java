e69d50aa4432174b7b47dac4f6017d94ddc11b02
andreacaduceo <75133289+andreacaduceo@users.noreply.github.com>
2021-11-24 16:16:44 +0100

    Add files via upload

---
+++ JobFinderApp/src/test/java/com/project/JobFinderApp/JobFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.JobFinderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
